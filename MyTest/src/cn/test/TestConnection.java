7da492de1c6f558f3c6a73de5ff36d53641b56fa
刘邓 <deng.liu@pinkitec.com>
2017-09-28 21:21:25 +0800

    提交

---
+++ MyTest/src/cn/test/TestConnection.java
@@ -0,0 +1,15 @@
+package cn.test;
+
+import java.net.URL;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+public class TestConnection {
+public void upload(String url,Map<String,String> map){
+	
+}
+public static void main(String[] args) {
+	
+}
+}
