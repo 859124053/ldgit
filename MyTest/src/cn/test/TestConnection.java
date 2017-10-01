886c5ffe02fbb0ea3c3f0213f01482afdf32b29a
刘邓 <deng.liu@pinkitec.com>
2017-10-02 00:52:32 +0800

    v

--- MyTest/src/cn/test/TestConnection.java
+++ MyTest/src/cn/test/TestConnection.java
@@ -12,9 +12,5 @@ public void upload(String url,Map<String,String> map){
 public static void main(String[] args) {
 System.out.println("TestConnection.main()");
 
-
-
-
-System.out.println("TestConnection.main()");
 }
 }
